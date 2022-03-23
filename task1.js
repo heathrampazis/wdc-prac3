f4a8cbd307f8a547913909fcdb09aa59afecfc41
Heath <heath@Heaths-MacBook-Pro.local>
2022-03-23 15:18:06 +1030

    task 3.2

--- task1.js
+++ task1.js
@@ -2,8 +2,6 @@ function myFunction() {
 
     var value = document.getElementById("current_time");
 
-    // var Date = new Date
-
     value.innerText = new Date();
 
 }
--- task3.js
+++ task3.js
@@ -1,11 +1,45 @@
 var count = 0;
 
-function myFunction() {
+function counter() {
     count = count + 1;
-    
+
     var box = document.getElementById("mcount");
 
     box.innerText = count;
 
     console.log("This worked");
+}
+
+function post() {
+
+    var posts = document.getElementById("posts");
+    var text = document.getElementById("te").value;
+    
+    //var post = document.getElementsByClassName("post_content");
+    //var time = document.getElementsByClassName("post_time");
+    
+    //var text = document.getElementsByTagName("textarea");
+
+    var date = new Date();
+
+    var t = document.createElement('P');
+    t.classList.add('post_time');
+    t.innerText = date;
+    posts.appendChild(t);
+
+    console.log(text.value);
+
+    var p = document.createElement('P');
+    p.classList.add("post_content");
+    p.innerText = text;
+    posts.appendChild(p);
+
+
+    //var test = document.createElement(time);
+
+    //time = date;
+
+    //posts.innerText = test + "\n" + post.value;
+    //posts.innerText = text.innerText;
+
 }
