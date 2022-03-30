a9d5b43177e16dd6ad888ad7c408de20a0ba5783
Heath <heath@Heaths-MBP.lan>
2022-03-30 19:44:25 +1030

    task3 stuff

--- task3.js
+++ task3.js
@@ -19,41 +19,50 @@ function counter() {
 
 function post() {
 
+    var t;
+    var p;
+  
     var posts = document.getElementById("posts");
-    var text = document.getElementById("te").value;
+    var text = document.getElementById("text").value;
 
-    var number_of_posts = document.getElementById("posts-amount").value;
+    var number_of_posts = document.getElementById("posts_amount").value;
 
     console.log(text.value);
 
     for (var i = 0; i < number_of_posts; i ++) {
       var date = new Date();
 
-      var t = document.createElement('P');
+      t = document.createElement('P');
       t.classList.add('post-time');
       t.innerText = date;
       posts.appendChild(t);
   
-      var p;
+
+      p = document.createElement('P');
+      p.classList.add("post-content");
+      p.innerText = text;
 
       if (document.getElementById("red_color").checked) {
-        p = document.createElement('P');
-        p.classList.add("post-content");
         p.classList.add("red");
-        p.innerText = text;
-        posts.appendChild(p);
       } else if (document.getElementById("blue_color").checked) {
-        p = document.createElement('P');
-        p.classList.add("post-content");
         p.classList.add("blue");
-        p.innerText = text;
-        posts.appendChild(p);
-      } else {
-        p = document.createElement('P');
-        p.classList.add("post-content");
-        p.innerText = text;
-        posts.appendChild(p);
       }
+
+      if (document.getElementById("bold_text").checked) {
+        p.classList.add("bold");
+      } 
+      
+      if (document.getElementById("italic_text").checked) {
+        p.classList.add("italic");
+      }
+
+      posts.appendChild(p);
+
+      post_numbers = post_numbers + number_of_posts;
+
+      // increase post numbers and slider reduces number of posts avaliable to be seen
+      // if number of posts seen is smaller than the current number of posts made hide posts
+
     }
 
 }
@@ -77,6 +86,7 @@ function hide2() {
 
     var x = document.getElementById("menu");
     var y = document.getElementById("main");
+
     if (x.style.display === "none") {
       x.style.display = "block";
       y.style.display = "none";
@@ -107,5 +117,31 @@ function bgcolor() {
   var bg_color = document.getElementById("background_color");
 
   bg_color.style.backgroundColor = background.value;
+
+}
+
+function hide_post() {
+
+  var slider = document.getElementById("slide_bar").value;
+
+  var posts = document.getElementById("posts").children;
+
+  // posts.children
+
+  console.log("Number of posts : " + post_numbers);
+  console.log("Post length : " + posts.length);
+
+  // for (var i = 0; i < slider; i ++) {
+
+  //   if (i > slider) {
+  //     posts[i].style.display = "none";
+  //     posts[i+1].style.display = "none";
+  //     i = i + 2;
+  //   } else {
+  //     posts[i].style.display = "block";
+  //     posts[i+1].style.display = "block";
+  //     i = i + 2;
+  //   }
+  // }
 
 }
